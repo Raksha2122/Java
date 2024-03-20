77bfb299433822704e4947fbe14d965e2f972226
Raksha Ashtankar <128995500+Raksha2122@users.noreply.github.com>
2024-03-20 21:21:55 +0530

    Add files via upload

---
+++ Java_datatypes.java
@@ -0,0 +1,28 @@
+import java.util.Scanner;
+
+public class Java_datatypes {
+    public static void main(String[] args) {
+        /*
+         * int rollno = 64; // primitive datatypes
+         * char letter = 'r';
+         * float marks = 98.8f;
+         * long large = 889776455543234L;
+         * Boolean check = false;
+         */
+        // Scanner input = new Scanner(System.in);
+        // System.out.println("Please enter some input");
+        // int rollno = input.nextInt();
+        // System.out.println("Your roll number " + rollno);
+        // int a = 10 ;//its literal and the reference variable is the identifiers
+        // String name = input.next();
+        // System.out.println(name);
+
+        // float marks = input.nextFloat();
+        // System.out.println(marks);
+        int a = 234_000_000; // commom is replaced with underscore or you can also use it as norma;
+        int b = 234000000;
+        System.out.println(a);
+        System.out.println(b);
+
+    }
+}
