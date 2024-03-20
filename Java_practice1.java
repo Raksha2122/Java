f11b31e9ca9cfd1f862f66eb780ddddac579885b
Raksha Ashtankar <128995500+Raksha2122@users.noreply.github.com>
2024-03-20 21:23:48 +0530

    Add files via upload

---
+++ Java_practice1.java
@@ -0,0 +1,42 @@
+//Type of Languages 
+// procedural series of well structured steps. eg java, python  
+// functional writing in pure functions, and you can use that many types, perform lot of operation with same datasets, First class function- reassigning eg python 
+// oject oriented (code+data = object),revolves around objects eg java, c++,python etc
+
+//static vs dynamic 
+// static perform type checking at compile time 
+// error will show at compile time 
+// declear datatype before you use it 
+// more 
+
+// dynamic perform type checking at runtime 
+// error might not show till program is run 
+//no need to declear datatype of variable 
+// saves time in writing code but might give error at runtime 
+
+//Two type of memory 
+// stack variable is stored here 
+// heap value of real object is stored here 
+// a is the stack memory pointing to words the object in the heap memory a=10
+// a=[1,3,5,9]
+//b=a
+// update a=[99,3,5,9]
+// o/p print b  o/p= [99,3,5,9]
+
+//How java code executes 
+//.java file (human readable)/ source code ---compiler--> .class file / byte code --- interpreter -->machine code (0 and 1)
+// this code will not directly run on the system 
+// we need java virtual machine to run it 
+
+// how java is platform independance 
+// byte code can run on all operating systems 
+// in java we get bytecode JVM converts this to machine code 
+// java is platform independent but JVM is platfrom dependent 
+
+//JDK java devlopment kit provides enviroment to develop and run the java program 
+// pack that include JRE to execute ur program, a compiler , archiver , docs generator , interpreter
+// JRE installation package that provides environment to only run the program
+
+// JAVA source code --->JDK [COMPLIER] --> BYTECODE --->JVM-----> JRE [JVM +HARDWare]
+system.out.print("Hello"); 
+
